e60c4abf37db02d58b7ecdd37e97288e4f7dbb8f
utkarsh singh <utkarshsingh122096@gmail.com>
2024-08-14 01:56:11 +0530

    fourth

--- src/main/java/org/example/TreeTraversal.java
+++ src/main/java/org/example/TreeTraversal.java
@@ -9,7 +9,7 @@ public class TreeTraversal {
 //              3                         11
 //        1           5            9               13
 //     0     2     4     6      8     10       12      14
-
+//7 3 11 13 9 5 1 0 2 4 6 8 10 12 14
 
         //////////////left/////////////////////////
         root.left = new Tree(3);
@@ -42,8 +42,89 @@ public class TreeTraversal {
         System.out.println();
         System.out.print("RIGHT VIEW TRAVERSAL  : ");
         rightViewTraversal(root);
+        System.out.println();
+        System.out.print("LEAF VIEW TRAVERSAL  : ");
+        leafViewTraversal(root);
+        System.out.println();
+        String string = (isPerfectBinaryTree(root)) ? "PERFECT BINARY TREE" : "NOT A PERFECT BINARY TREE";
+        System.out.print("GIVEN BINARY TREE IS : "+string);
+        System.out.println();
+        System.out.print("ZIK ZAK TRAVERSAL  : ");
+        zikZakTraversal(root);
 
+    }
 
+    private static void zikZakTraversal(Tree root) {
+        int depth = findDepth(root);
+        for(int i=0;i<=depth;i++){
+            if(i%2==0){
+                leftToRight(root,i);
+            }else {
+                rightToLeft(root,i);
+            }
+        }
+    }
+
+    private static void rightToLeft(Tree root, int i) {
+        int d=0;
+        if(root.right!=null){rightToLeft(root.right,i);}
+        if(d==i){
+            System.out.print(root.val);
+            System.out.print(" -> ");
+        }
+        if(root.left!=null){rightToLeft(root.left,i);}
+
+    }
+
+    private static void leftToRight(Tree root, int i) {
+        int d=0;
+        if(root.left!=null){leftToRight(root.left,i);}
+        if(d==i){
+            System.out.print(root.val);
+            System.out.print(" -> ");
+        }
+        if(root.right!=null){leftToRight(root.right,i);}
+    }
+
+    private static boolean isPerfectBinaryTree(Tree root) {
+        int depth = findDepth(root);
+        int numOfNodes = countNodes(root);
+
+        int nOfn = (int) Math.pow(2, (depth))-1;
+        System.out.println(nOfn +" "+depth+" "+numOfNodes);
+        if (numOfNodes == nOfn){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private static int countNodes(Tree root) {
+        if(root==null){
+            return 0;
+        }
+        int  left = countNodes(root.left);
+        int right =  countNodes(root.right);
+        return  (1+left + right);
+    }
+
+    private static int findDepth(Tree root) {
+        int c=0;
+        while(root!=null){
+            c++;
+            root = root.left;
+        }
+        return  c;
+    }
+
+    private static boolean binaryTreeRec(Tree root, int depth, int i) {
+        if (root==null){
+            return  true;
+        }
+        if (root.left != null && root.right != null ){
+            return (depth == i);
+        }
+        return false;
     }
 
     public static void preOrderTraversal(Tree root) {
@@ -116,14 +197,16 @@ public class TreeTraversal {
         }
     }
 
-    public static void ViewTraversal(Tree root) {
+    public static void leafViewTraversal(Tree root) {
         if(root==null){
             return;
         }
-        System.out.print(root.val);
-        if(root.right!=null){
+        if(root.left==null && root.right==null){
+            System.out.print(root.val);
             System.out.print(" -> ");
-            rightViewTraversal(root.right);
         }
+        leafViewTraversal(root.left);
+        leafViewTraversal(root.right);
     }
+    
 }
